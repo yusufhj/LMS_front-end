cba4bef1d17a7860f434a255b0ef3587149d6f2f
alsaegh98 <abdullajaffer8@gmail.com>
2024-11-18 10:50:53 +0300

    added the needed folders and files for the front end and created a NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,25 +1,42 @@
 import { Link } from 'react-router-dom';
+import { AuthedUserContext } from '../../App';
+import { useContext } from 'react';
 
-const NavBar = ({ user, handleSignout }) => {
+const NavBar = ({ handleSignout }) => {
+  const user = useContext(AuthedUserContext);
   return (
     <>
-      { user ? (
-        <nav>
+      {user ? (
+        
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="" onClick={handleSignout}>Sign Out</Link></li>
+            <li>Welcome, {user.username}</li>
+            <li>
+              <Link to="/">HOME</Link>
+            </li>
+            <li>
+              <Link to='/courses'>courses</Link>
+            </li>
+            <li>
+              <Link to='/courses/new'>NEW COURSE</Link>
+            </li>
+            <li>
+              <Link to="" onClick={handleSignout}>
+                Sign Out
+              </Link>
+            </li>
           </ul>
-        </nav>
+     
       ) : (
-        <nav>
           <ul>
-            <li><Link to="/signin">Sign In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
+            <li>
+              <Link to="/signin">Sign In</Link>
+            </li>
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
           </ul>
-        </nav>
       )}
     </>
-  )
-}
-
+  );
+};
 export default NavBar;
