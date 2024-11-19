12aaa171a98ef25c2d97f74085ea42101018193d
alsaegh98 <abdullajaffer8@gmail.com>
2024-11-19 12:38:59 +0300

    added a lesson inside course

--- src/components/LessonForm/LessonForm.jsx
+++ src/components/LessonForm/LessonForm.jsx
@@ -1,42 +1,54 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-const LessonForm = ({ handleAddLesson }) => {
+const LessonForm = () => {
   const [formData, setFormData] = useState({
     title: '',
     content: '',
-  })
+  });
+
+  const [lessons, setLessons] = useState([]);
 
   const handleChange = (event) => {
-    setFormData({ ...formData, [event.target.name]: event.target.value })
-  }
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
 
   const handleSubmit = (event) => {
-    event.preventDefault()
-    handleAddLesson(formData)
-    setFormData({ title: '', content: '' }) 
-  }
+    event.preventDefault();
+    setLessons([...lessons, formData]);
+    setFormData({ title: '', content: '' });
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="lesson-title">Lesson Title:</label>
-      <input
-        type="text"
-        name="title"
-        id="lesson-title"
-        value={formData.title}
-        onChange={handleChange}
-        required
-      />
-      <label htmlFor="lesson-content">Lesson Content:</label>
-      <textarea
-        name="content"
-        id="lesson-content"
-        value={formData.content}
-        onChange={handleChange}
-      ></textarea>
-      <button type="submit">ADD LESSON</button>
-    </form>
-  )
-}
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="lesson-title">Lesson Title:</label>
+        <input
+          type="text"
+          name="title"
+          id="lesson-title"
+          value={formData.title}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="lesson-content">Lesson Content:</label>
+        <textarea
+          name="content"
+          id="lesson-content"
+          value={formData.content}
+          onChange={handleChange}
+        ></textarea>
+        <button type="submit">ADD LESSON</button>
+      </form>
+      <ul>
+        {lessons.map((lesson, index) => (
+          <li key={index}>
+            <h2>{lesson.title}</h2>
+            <p>{lesson.content}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default LessonForm
+export default LessonForm;
